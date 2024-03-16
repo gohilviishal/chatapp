689463ef8e8965498b3c2ddb153a5bf23028bf04
gohilviishal <viishalgohil193@gmail.com>
2024-03-16 14:24:35 +0530

    Change Deploy origin

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ const server = app.listen(PORT, () => {
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://ituthread.onrender.com",
   },
   pingTimeout: 60000,
 });
