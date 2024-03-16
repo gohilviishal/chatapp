b8b7a3ab97a83e86cce028d49586a9ee8e12d812
gohilviishal <viishalgohil193@gmail.com>
2024-03-16 14:28:51 +0530

    Remove api console middleware

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method);
+//   next();
+// });
 
 app.use("/api/user", userRoute);
 app.use("/api/chat", chatRoute);
